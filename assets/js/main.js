3b68d489c0a750b6f0a1ea7fe092d51c5e09700e
Mahipal, Malviya <mahipal.malviya@cerence.com>
2023-06-15 00:30:20 +0530

    - Update profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@
           var custom_greet = document.querySelector('#intro-greeting');
           custom_greet.style.display = 'block';
           var fullName = document.querySelector('#full-name');
-          fullName.innerHTML = 'Uday Kanth Reddy';
+          fullName.innerHTML = 'Mahipal Malviya';
           $('#header').removeClass('header-top');
           $('section').removeClass('section-show');
           return;
@@ -39,7 +39,7 @@
           console.log('Header-top activated');
 
           var fullName = document.querySelector('#full-name');
-          fullName.innerHTML = 'Uday Kanth Reddy Kakarla';
+          fullName.innerHTML = 'Mahipal Malviya';
 
           var custom_greet = document.querySelector('#intro-greeting');
           custom_greet.style.display = 'none';
